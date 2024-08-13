412570eb6e459bf2affdb4f68dee07af9aff524f
Temirlan Yelemanov <36527788+TiTaniumum@users.noreply.github.com>
2024-08-13 21:48:57 +0500

    test commit

--- Front/MishFit/pages/Home.tsx
+++ Front/MishFit/pages/Home.tsx
@@ -9,6 +9,7 @@ export default function Home({ navigation }: any) {
     <View style={styles.container}>
         <Text>Hello World</Text>
         <Text>Hello everyone</Text>
+        <Text>Task1 text</Text>
     </View>
   );
 }
