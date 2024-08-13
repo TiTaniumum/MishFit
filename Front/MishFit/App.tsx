eff9fa4a04524be4b2de22b90f3bacc3ff9ea6f3
Temirlan Yelemanov <36527788+TiTaniumum@users.noreply.github.com>
2024-08-13 20:30:31 +0500

    Base installations done

--- Front/MishFit/App.tsx
+++ Front/MishFit/App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ContextProvider } from "./components/ContextProvider";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./pages/Home";
+
+const RootStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ContextProvider>
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Group>
+            <RootStack.Screen
+              name="Home"
+              component={Home}
+              options={({ navigation }) => ({
+                
+              })}
+            />
+          </RootStack.Group>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </ContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
